cadc55fe00947f57973579fc2827a4911b28c494
Namrata Jaiswal <namratajaiswal047@gmail.com>
2025-04-28 02:46:02 +0530

    Commit

--- countries/src/api/api.jsx
+++ countries/src/api/api.jsx
@@ -1,12 +1,12 @@
 import axios from "axios"
 
-export const BACKENDPOINT="https://restcountries.com/"
+export const BACKENDPOINT="https://xcountries-backend.azurewebsites.net/all"
 
 export default async function countriesData(){
     try{
         const response= await axios.get(`${BACKENDPOINT}/v3.1/all`)
         return response.data
     }catch(e){
-        console.error(e)
+        console.error("Error fetching data:")
     }
 }

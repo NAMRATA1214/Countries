3cf0629fb131736939960a415dbd7ed77a1076e9
Namrata Jaiswal <namratajaiswal047@gmail.com>
2025-04-28 23:15:33 +0530

    Commit

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Countries from './Countries/Countries';
+import Countries from "./Countries";
 
 function App() {
   return (
     <div className="App">
-      <Countries/>
+      <Countries />
     </div>
   );
 }
